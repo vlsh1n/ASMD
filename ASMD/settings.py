83920d869d1578b6bd4f779e2bb2af6eed917365
vlsh1n <voloshinw@gmail.com>
2024-11-23 03:14:01 +0200

    env and react initial

--- ASMD/settings.py
+++ ASMD/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3-ug24w!0$_2ny+$$xm9i+-f4a4e5rxs$e=xcs)a*2wda%($1p'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
